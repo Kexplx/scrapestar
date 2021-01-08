3c4b8eccf4d90dbc9853d7c92c8d4e3c91cd1c6c
Oscar Rosner <oscar.rosner@web.de>
2021-01-09 00:54:01 +0100

    ui: remove A11y module

--- ui/src/app/shared/cdk.module.ts
+++ ui/src/app/shared/cdk.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-import { A11yModule } from '@angular/cdk/a11y';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
-  exports: [A11yModule, ClipboardModule],
+  exports: [ClipboardModule],
 })
 export class CdkModule {}
