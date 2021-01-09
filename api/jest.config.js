533898a7e6215fb7a2d6b2314ace8c56c68d3eb5
Oscar Rosner <oscar.rosner@web.de>
2021-01-09 22:00:47 +0100

    api: setup jest

---
+++ api/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // compile .ts files with ts-jest compiler.
+  },
+};
---
+++ api/src/scheduler/job-scheduler.test.ts
@@ -0,0 +1,61 @@
+import { ExecutionTime } from '../interfaces/execution-time';
+import { Job } from '../job';
+import { JobScheduler } from './job-scheduler';
+
+let storeMock: any = { updateResult: jest.fn(async () => {}) };
+let scheduler = JobScheduler.getInstance(storeMock as any);
+
+beforeEach(() => {
+  // 04.01.2021 8:25
+  const monday8h25m = new Date(2021, 0, 4, 8, 25);
+
+  jest.useFakeTimers('modern');
+  jest.setSystemTime(monday8h25m.getTime());
+
+  storeMock = { updateResult: jest.fn(async () => {}) };
+  scheduler = JobScheduler.getInstance(storeMock as any);
+});
+
+describe('#schedule', () => {
+  it('should call #execute on the job after 5 and 10 minutes', done => {
+    const fiveMinutes = 5 * 60000;
+    const executionTimes = [
+      { dayOfWeek: 1, hour: 8, minute: 30 },
+      { dayOfWeek: 1, hour: 8, minute: 35 },
+    ];
+    const jobMock = createJobMock(executionTimes);
+
+    scheduler.schedule(jobMock);
+    expect(jobMock.execute).not.toBeCalled(); // No call.
+
+    jest.advanceTimersByTime(fiveMinutes);
+    expect(jobMock.execute).toHaveBeenCalledTimes(1); // One call after 5 min.
+
+    jest.advanceTimersByTime(fiveMinutes);
+    expect(jobMock.execute).toHaveBeenCalledTimes(2); // Two calls after 10 min.
+    done();
+  });
+
+  it('should not call #execute on a cancelled job', done => {
+    const fiveMinutes = 5 * 60000;
+    const executionTimes = [{ dayOfWeek: 1, hour: 8, minute: 30 }];
+    const jobMock = createJobMock(executionTimes);
+
+    scheduler.schedule(jobMock);
+    scheduler.cancel(jobMock.id);
+
+    jest.advanceTimersByTime(fiveMinutes);
+
+    expect(jobMock.execute).not.toHaveBeenCalled();
+    done();
+  });
+});
+
+function createJobMock(executionTimes: ExecutionTime[]): Job {
+  return {
+    executionTimes,
+    id: Math.random().toString(),
+    dto: { executionResult: 'RESULT' },
+    execute: jest.fn(async () => {}),
+  } as any;
+}
