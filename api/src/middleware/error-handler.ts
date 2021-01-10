18c83a3978461aae94f486e94c3b186a36fa6fed
Oscar Rosner <oscar.rosner@web.de>
2021-01-10 06:37:51 +0100

    api: update types

--- api/src/middleware/error-handler.ts
+++ api/src/middleware/error-handler.ts
@@ -1,7 +1,7 @@
-import { Handler, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 // Default error handler for all uncaught exceptions.
-export function errorHandler(err: Error, _req: Request, res: Response, _next: Handler) {
+export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction) {
   console.error(err.stack);
   res.status(400).send('Bad reqest.');
 }
