dc3c029f627cf99018c647bbe8346ea0757c775e
Oscar Rosner <oscar.rosner@web.de>
2021-01-10 06:40:03 +0100

    api: folder

--- api/src/job/job.ts
+++ api/src/job/job.ts
@@ -1,14 +1,14 @@
-import { ExecutionTime } from './interfaces/execution-time';
-import { JobDto } from './interfaces/job-dto';
-import { ExecutionResult } from './interfaces/execution-result';
-import { Selector } from './interfaces/selector';
 import puppeteer from 'puppeteer';
+import { ExecutionResult } from '../interfaces/execution-result';
+import { ExecutionTime } from '../interfaces/execution-time';
+import { JobDto } from '../interfaces/job-dto';
+import { Selector } from '../interfaces/selector';
 
 export class Job {
   private _id?: string;
   private _url: string;
-  private _selectors: Selector[];
   private _name: string;
+  private _selectors: Selector[];
   private _executionTimes: ExecutionTime[];
   private _executionResult?: ExecutionResult;
 
@@ -58,30 +58,38 @@ export class Job {
    * 3. Saves the scrape result in `result`
    *
    * @example
-   * await job.execute();
-   * job.result; // Results of the last execution.
+   * await job.execute()
+   * job.dto.executionResult // Results of the last execution.
    */
   async execute(): Promise<void> {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(this._url);
+
+    // Don't crash on invalid url.
+    try {
+      await page.goto(this._url);
+    } catch (error) {
+      browser.close();
+      return;
+    }
 
     const result: ExecutionResult = await page.evaluate((selectorsAsJson: string) => {
       // We are inside the pages context here.
       // Can use the browser APIs like `document` or `window`.
-      // Can NOT use any variables or methods from outside this block.
+      // Can NOT use any variables or methods from outside this callback.
       const selectors: Selector[] = JSON.parse(selectorsAsJson);
       const scrapeResult: ExecutionResult = {};
 
       for (const { cssSelector, name } of selectors) {
         const textContent = document.querySelector(cssSelector)?.textContent ?? '';
 
-        scrapeResult[name] = textContent;
+        scrapeResult[name] = textContent.replace(/\s\s+/g, ' '); // Remove newlines, tabs and consecutive spaces.
       }
 
       return scrapeResult;
     }, JSON.stringify(this._selectors));
 
     this._executionResult = result;
+    browser.close();
   }
 }
