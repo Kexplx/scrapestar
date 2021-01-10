8b3d437cbf48528771a03f5160eebb088e2836ba
Oscar Rosner <oscar.rosner@web.de>
2021-01-10 06:39:22 +0100

    .

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import { JobDtoStore } from './job-dto-store';
-import { Job } from './job';
-import { JobScheduler } from './job-scheduler';
+import { JobScheduler } from './scheduler/job-scheduler';
 import { router as jobRouter } from './routes/jobs';
 import { errorHandler } from './middleware/error-handler';
+import cors from 'cors';
+import { Job } from './job/job';
+
+const PORT = 3000;
 
 (async () => {
-  const PORT = 3000;
-
   const app = express();
-  const store = JobDtoStore.getInstance();
-  const scheduler = JobScheduler.getInstance(store);
+
+  const store = new JobDtoStore();
+  const scheduler = new JobScheduler(store);
 
   // Load all jobs from the store and schedule them.
   const jobDtos = await store.findAll();
@@ -21,10 +23,12 @@ import { errorHandler } from './middleware/error-handler';
     scheduler.schedule(job);
   }
 
-  // Setup routes and middleware.
-  app.use('/jobs', jobRouter);
+  // Setup middleware and routes.
+  app.use(cors());
   app.use(errorHandler);
   app.use(express.json());
+
+  app.use('/jobs', jobRouter);
 
   app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
 })();
