465decc79a5b43767d437c3794710b4d73c287fe
Oscar Rosner <oscar.rosner@web.de>
2021-01-10 06:42:38 +0100

    ui: update source link

--- ui/src/app/job-card/job-card.component.ts
+++ ui/src/app/job-card/job-card.component.ts
@@ -8,7 +8,7 @@ import { Job } from '../models/job';
   templateUrl: './job-card.component.html',
   styleUrls: ['./job-card.component.scss'],
 })
-export class JobCardComponent implements OnInit {
+export class JobCardComponent {
   @Input() job!: Job;
   @Output() editClicked = new EventEmitter();
   @Output() deleteClicked = new EventEmitter();
@@ -17,12 +17,6 @@ export class JobCardComponent implements OnInit {
   tabIndex = 0;
 
   constructor(private message: NzMessageService) {}
-
-  ngOnInit() {
-    if (this.job.executionTimes.length) {
-      this.nextExecution = this.nextExecutionDate(this.job.executionTimes);
-    }
-  }
 
   onCopyClipboard() {
     this.message.success('Endpoint copied to clipboard');
@@ -34,33 +28,5 @@ export class JobCardComponent implements OnInit {
 
   onDelete() {
     this.deleteClicked.emit();
-  }
-
-  private nextExecutionDate(exectuionTimes: ExecutionTime[]): Date {
-    const [nextExecutionTimeAsDate] = exectuionTimes
-      .map(et => this.getDateFromExecutionTime(et))
-      .sort((a, b) => a.getTime() - b.getTime())
-      .filter(a => a.getTime() > Date.now());
-
-    return nextExecutionTimeAsDate;
-  }
-
-  private getDateFromExecutionTime(executionTime: ExecutionTime): Date {
-    const { dayOfWeek, minute, hour } = executionTime;
-    const now = new Date();
-    const then = new Date();
-
-    let daysDiff;
-    if (now.getDay() > dayOfWeek) {
-      daysDiff = dayOfWeek + 7 - now.getDay();
-    } else {
-      daysDiff = dayOfWeek - now.getDay();
-    }
-
-    then.setDate(now.getDate() + daysDiff);
-    then.setMinutes(minute);
-    then.setHours(hour);
-
-    return then;
   }
 }
